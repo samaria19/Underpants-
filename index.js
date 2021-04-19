92a5d9cda0d842289c98d8111847bbf40d272b7e
Jonathan Corley <>
2021-04-19 15:47:22 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,81 @@
+var _ = {};
+
+
+/**
+* START OF OUR LIBRARY!
+* Implement each function below it's instructions
+*/
+
+/** _.identity
+* Arguments:
+*   1) Any value
+* Objectives:
+*   1) Returns <value> unchanged
+* Examples:
+*   _.identity(5) === 5
+*   _.identity({a: "b"}) === {a: "b"}
+*/
+_.identity = function(value){
+//CODE BELOW HERE
+
+
+
+
+//CODE ABOVE HERE
+}
+
+/** _.typeOf
+* Arguments:
+*   1) Any value
+* Objectives:
+*   1) Return the type of <value> as a string
+*       Types are one of:
+*          - "string"
+*          - "array"
+*          - "object"
+*          - "undefined"
+*          - "number"
+*          - "boolean"
+*          - "null"
+*          - "function"
+* Examples:
+* _.typeOf(134) -> "number"
+* _.typeOf("javascript") -> "string"
+* _.typeOf([1,2,3]) -> "array"
+*/
+
+_.typeOf = function (value){
+//CODE BELOW HERE
+
+
+
+
+//CODE ABOVE HERE
+}
+
+/** _.first
+* Arguments:
+*   1) An array
+*   2) A number
+* Objectives:
+*   1) If <array> is not an array, return []
+*   2) If <number> is not given or not a number, return just the first element in <array>.
+*   3) Otherwise, return the first <number> items of <array>
+* Edge Cases:
+*   1) What if <number> is negative?
+*   2) What if <number> is greater than <array>.length?
+* Examples:
+*   _.first("ponies", 1) -> []
+*   _.first(["a", "b", "c"], "ponies") -> "a"
+*   _.first(["a", "b", "c"], 1) -> "a"
+*   _.first(["a", "b", "c"], 2) -> ["a", "b"]
+*/
+
+_.first = function(array, number){
+//CODE BELOW HERE
+
+
+
+
+//CODE ABOVE HERE
+};
